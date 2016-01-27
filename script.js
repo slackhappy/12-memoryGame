114de4e298d5a8e93c7e1cdf554566f8f35d7c09
Sebasking01 <noordinarypeopleallowed@gmail.com>
2016-01-27 16:27:18 -0500

    removed solutionc code

--- script.js
+++ script.js
@@ -1,105 +1,75 @@
-// This function returns the selection of the computer
 var userPoint = 0;
-var faceUp = []
+var faceUp = [];
 var assignments = 1;
-var divIds = []
-var matches = 0
+var divIds = [];
+var matches = 0;
 
 function shufflePositions() {
-  //TODO: randomly assign positions to the tiles on the screen
-  var choices = ['rock', 'rock', 'paper', 'paper', 'scissors', 'scissors'];
-  var shuffled = shuffle(choices)
-  shuffled.forEach(addClassesToDivs)
+  //TODO: randomly assign positions to the tiles on the screen call
+  // addTileToBoard here
+  // Hint: You can use a forEach or a for loop
 
-}
+};
 
 function shuffle(array) {
   //TODO: use this method to shuffle the array
-  var newArray = []
-  var length = array.length
+  var newArray = [];
+  var length = array.length;
 
   for(i = 0; i < length; i++) {
-    var arrLen = array.length
+    var arrLen = array.length;
     var randomPick = Math.floor(Math.random() * arrLen);
-    var element = array[randomPick]
-    newArray.push(element)
-    array.splice(array.indexOf(element), 1)
-    arrLen --
+    var element = array[randomPick];
+    newArray.push(element);
+    array.splice(array.indexOf(element), 1);
+    arrLen --;
   }
 
-  return newArray
-}
+  return newArray;
+};
 
-function addClassesToDivs(element) {
+function addTileToBoard(element) {
   //TODO: randomly assign ids to the divs
-  var div =  "<div id=\"" + assignments +"\" class=\""+ element + " token\">Worked</div>"
+  var div =  "<div id=\"" + assignments +"\" class=\""+ element + " token\"></div>";
+  $(".game").append(div);
   assignments ++
-  $(".game").append(div)
-}
+};
 
 function clearBoard() {
-  $(".game").empty()
-}
+  $(".game").empty();
+};
 
-function checkCardMatch(array) {
-  //TODO: check to see if the value of the 2 face up cards match
-  if (array[0] === array[1]) {
-    window.x = array[0]
-    matches ++
-    $("#" + divIds[0]).addClass("matched")
-    $("#" + divIds[1]).addClass("matched")
-    divIds = []
-    console.log("Found match!")
-  } else {
-    divIds = []
-    console.log("No match")
-  }
-}
+function checkTilesMatch(array) {
+  //TODO: check to see if the value of the 2 face up tile match
 
-function assignImages() {
- //TODO: once the html tiles are shuffled dynamically add images to them
-}
+};
 
 function setPoint() {
     $('#userPoint').text(userPoint);
-}
+};
 
 function evaluate( evt ) {
-    var userValue = evt.target.getAttribute('class');
+    var tileType = evt.target.getAttribute('class');
     var divID = evt.target.getAttribute("id");
-    faceUp.push(userValue)
-    divIds.push(divID)
+    //TODO: If this is first tile clicked store the card if it is the second tile
+    // clicked check to see if it is the same as the first.
+};
 
-    if (faceUp.length == 2) {
-      checkCardMatch(faceUp)
-      faceUp = []
-    }
 
-    if (matches == 3) {
-      alert("You win!")
-      matches = 0
-      clearBoard()
-      playGame()
-    }
-}
+$(document).ready(function() {
+  function bop(elm) {
+      $(elm).fadeOut('fast').delay(1).fadeIn('fast');
+  };
 
-function playGame() {
   shufflePositions();
-  $("#card").flip({
-    trigger: 'click'
-  })
-    function bop(elm){
-        $(elm).fadeOut('fast').delay(1).fadeIn('fast');
-    }
 
-    $('.token').click( function(e) {
-
-        bop(this);
-        evaluate(e);
-    });
-}
+  $('.token').click( function(e) {
+      var element = e.toElement.classList[0]
+      var id = e.toElement.getAttribute("id")
+      assignText(id, element)
+      bop(this);
+      evaluate(e);
+  });
 
 
-$(document).ready(function(){
-  playGame()
 });
